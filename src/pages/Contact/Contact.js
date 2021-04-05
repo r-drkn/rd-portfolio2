2cc8aa761216c78cd73f5401da8ab92ad243b36b
r-drkn <robert.dirken@gmail.com>
2021-04-05 15:25:25 +1000

    Filled in the rest of contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -10,6 +10,7 @@ import linkedin from "../../icons/linkedin.svg";
 import mail from "../../icons/mail.svg";
 import resume from "../../icons/resume.svg";
 import resumeFile from "../../images/RobertDirkenResumé.pdf";
+import theme from "../../theme";
 
 const contactListItems = [
   { item: "Mail", url: "mailto:robert.dirken@gmail.com", image: mail },
@@ -51,10 +52,15 @@ function ContactListItem(props) {
 }
 
 export default function Contact() {
+  const { secondary } = theme.colors;
+
   return (
     <div
       id="contact"
       sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         height: "100vh",
         maxWidth: "900px",
         width: "100%",
@@ -86,6 +92,30 @@ export default function Contact() {
           );
         })}
       </ul>
+
+      <div>
+        <p sx={{ fontSize: "2rem", textAlign: "center", pb: "2rem" }}>
+          Please don't hesitate to reach out about any opportunities regardless
+          how big or small always looking for new projects, collaborations or
+          even just chats about web things!
+        </p>
+      </div>
+      <svg
+        width="283"
+        height="28"
+        viewBox="0 0 283 28"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 14C12.3333 0.666667 20.6667 0.666667 29 14C37.3333 27.3333 45.6667 27.3333 54 14C62.3333 0.666667 70.6667 0.666667 79 14C87.3333 27.3333 95.6667 27.3333 104 14C112.333 0.666667 120.667 0.666667 129 14C137.333 27.3333 145.667 27.3333 154 14C162.333 0.666667 170.667 0.666667 179 14C187.333 27.3333 195.667 27.3333 204 14C212.333 0.666667 220.667 0.666667 229 14C237.333 27.3333 245.667 27.3333 254 14C262.333 0.666667 270.667 0.666667 279 14"
+          stroke={secondary}
+          stroke-width="8"
+        />
+      </svg>
+      <h4 sx={{ fontFamily: "code", fontWeight: 200, py: "4rem" }}>
+        Copyright© Robert Dirken 2021
+      </h4>
     </div>
   );
 }
