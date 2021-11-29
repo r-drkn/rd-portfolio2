89030e420911b4c78ffb43672f0ceee5d09c5fc6
Rob <rob.dirken@growthops.com.au>
2021-11-30 06:06:55 +1000

    fix: updates landing copy

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -8,7 +8,6 @@ import { landingAnimate } from "./landingAnimate.js";
 import gsap from "gsap";
 import theme from "../../theme.js";
 import MainContainer from "../../components/MainContainer.js";
-// import selfie from "../../images/selfie-test.png";
 import { useMediaQuery } from "react-responsive";
 import LandingImage from "./LandingImage.js";
 
@@ -33,31 +32,9 @@ export default function Landing(props) {
 
   return (
     <MainContainer>
-      {/* <svg
-        width="1280"
-        height="549"
-        viewBox="0 0 1280 549"
-        xmlns="http://www.w3.org/2000/svg"
-        sx={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          zIndex: -9999,
-        }}
-      >
-        <path d="M-5 0H1285V273H-5V0Z" fill={primary} />
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M0 388L15 395C30 401 61 414 91 433C122 452 152 478 183 491C213 503 244 503 274 484C305 465 335 427 366 401C396 375 427 363 457 350C488 337 518 324 549 331C579 337 610 363 640 401C670 439 701 491 731 478C762 465 792 388 823 375C853 363 884 414 914 465C945 516 975 567 1006 542C1036 516 1067 414 1097 388C1128 363 1158 414 1189 433C1219 452 1250 439 1265 433L1280 427V273H1265C1250 273 1219 273 1189 273C1158 273 1128 273 1097 273C1067 273 1036 273 1006 273C975 273 945 273 914 273C884 273 853 273 823 273C792 273 762 273 731 273C701 273 670 273 640 273C610 273 579 273 549 273C518 273 488 273 457 273C427 273 396 273 366 273C335 273 305 273 274 273C244 273 213 273 183 273C152 273 122 273 91 273C61 273 30 273 15 273H0V388Z"
-          fill={primary}
-        />
-      </svg> */}
       <div
         sx={{
           width: "100%",
-
           position: "relative",
           maxWidth: "900px",
           mx: "auto",
@@ -124,8 +101,7 @@ export default function Landing(props) {
               lineHeight: ["1.5rem", "2rem"],
             }}
           >
-            I'm a Junior Web Developer living in Brisbane with a particular
-            interest in React and Front End development.
+            I'm a Web Developer living in Brisbane.
           </p>
         </div>
         {!isMobile && (
@@ -156,7 +132,6 @@ export default function Landing(props) {
             </svg>
           </div>
         )}
-        {/* <LookingForWork gridArea="6 / 13 / 8 / 16 " /> */}
       </div>
     </MainContainer>
   );
